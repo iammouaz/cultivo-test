a86d6383d75d3f339c180fe5d4d6f61fd19be968
iammouaz <88675267+iammouaz@users.noreply.github.com>
2024-06-14 17:25:09 +0300

    run tests

---
+++ core/cypress/e2e/event-bid-login-22.cy.js
@@ -0,0 +1,52 @@
+const loginNumber = 2;
+
+describe('Event - Bid', () => {
+
+    beforeEach(() => {
+        cy.login(loginNumber);
+    });
+
+    function bidAutobidTest() {
+        cy.visit(Cypress.env('EVENT_URL'));
+    
+        cy.wait(1000);
+
+        for (let i = 0; i < 40; i++) {
+            cy.get('.cmn--btn.btn--sm.bid_now.bid-with-anim').then($buttons => {
+                const randomIndex = Math.floor(Math.random() * $buttons.length);
+                cy.wrap($buttons[randomIndex]).click();
+            });
+            cy.wait(2000);
+            cy.get('.modal_submit').click();
+            cy.wait(2000);
+            cy.get('#bid_form > .modal-footer > .btn--base').click();
+            cy.wait(5000);
+            cy.get('.cmn--btn.btn--sm.auto_bid_now.bid-with-anim').then($buttons => {
+                const randomIndex = Math.floor(Math.random() * $buttons.length);
+                cy.wrap($buttons[randomIndex]).click();
+            });
+            cy.wait(500);
+            
+            cy.get('.d-flex.final-total-price.animate-value > .text-truncate').first().should('exist').invoke('text').then((priceText) => {
+                const price = parseFloat(priceText.replace(/[^0-9.-]+/g, "")); // Remove any non-numeric characters
+                const max_bid = price + 10;
+                cy.get('#max_bid_in_').type(max_bid.toString());
+            });
+            cy.wait(500);
+            
+            cy.get('#bidding_step_').type('0.1');
+            cy.wait(500);
+            cy.get('#autobid_form > .modal-footer > .btn--base').click();
+            cy.wait(5000);
+
+        }
+        
+
+    }
+
+    Cypress._.times(7, (i) => {
+        it(`Bid - Autobid Iteration ${i + 1}`, () => {
+            bidAutobidTest();
+        });
+    });
+});
